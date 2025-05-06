3815e3673a21cbd4d5edbee6ba5daeaf92c829d1
Souhail Chaiberras <souhail.chaiberras@gmail.com>
2025-05-06 02:49:42 +0200

    first push

---
+++ service/ApiGateway/Program.cs
@@ -0,0 +1,55 @@
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace ApiGateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = builder.Configuration["Jwt:Authority"];
+        options.Audience = builder.Configuration["Jwt:Audience"];
+
+        // For development (bypass HTTPS validation)
+        options.BackchannelHttpHandler = new HttpClientHandler
+        {
+            ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+        };
+    });
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Configuration.AddJsonFile("ocelot.json");
+            builder.Services.AddOcelot(builder.Configuration);
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+            app.UseOcelot().Wait();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ service/UserService/Dtos/UpdateProfesseurDto.cs
@@ -0,0 +1,15 @@
+﻿namespace UserService.Dtos
+{
+    public class UpdateProfesseurDto
+    {
+        public string? Nom { get; set; }
+        public string? Prenom { get; set; }
+        public string? Photo { get; set; }
+        public string? Departement { get; set; }
+        public string? ModulesEnseignes { get; set; }
+        public string? Matricule { get; set; }
+        public int? AnneesDExperience { get; set; }
+        public string? Filieres { get; set; }
+        public string? Specialite { get; set; }
+    }
+}
