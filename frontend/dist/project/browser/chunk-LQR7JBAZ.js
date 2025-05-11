c174b2aea0dc1e0c3f89f092a92f9a903c65aac6
OUMIF <eladnanimehdi03@gmail.com>
2025-05-11 02:34:09 +0100

    Remove ignored files from Git tracking

--- frontend/dist/project/browser/chunk-LQR7JBAZ.js
+++
@@ -1,1 +0,0 @@
-import{Ia as t,Xa as n,Ya as p,kb as r}from"./chunk-GGVYOOZC.js";import"./chunk-EQDQRRRY.js";var s=class o{static \u0275fac=function(e){return new(e||o)};static \u0275cmp=t({type:o,selectors:[["app-prof"]],decls:2,vars:0,template:function(e,f){e&1&&(n(0,"p"),r(1,"prof works!"),p())},encapsulation:2})};export{s as ProfComponent};
--- frontend/dist/project/server/chunk-7E26J6GC.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{La as t,ab as n,bb as p,pb as r}from"./chunk-JR6AME7G.mjs";import"./chunk-S6KH3LOX.mjs";var s=class o{static \u0275fac=function(e){return new(e||o)};static \u0275cmp=t({type:o,selectors:[["app-prof"]],decls:2,vars:0,template:function(e,f){e&1&&(n(0,"p"),r(1,"prof works!"),p())},encapsulation:2})};export{s as ProfComponent};
